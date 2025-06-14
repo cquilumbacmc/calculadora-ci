02830ad8daf75d7cf9bfe73929c0bbccd5821c30
cquilumbacmc <carlosquilumba.clearminds@gmail.com>
2025-06-14 03:13:03 -0500

    feat: implementacion de la funcionalidad sumar

--- calculator.js
+++ calculator.js
@@ -1,7 +1,34 @@
-class Calculator {
+var Calculator = class {  
+    constructor() {
+        this.result = 0; 
+
     sum(a, b) {
-        return a + b;
+        if(a == null || b == null) {  
+            return 0;
+        }
+        
+        var result = a + b; 
+        
+        console.log("El resultado es: " + result);  
+        
+        return result;
+    }
+
+    
+    unusedMethod() {
+        let x = 1;
+        let y = 2;
+        return x + y;
     }
 }
-
+}
 module.exports = Calculator;
+
+
+//class Calculator {
+ //   sum(a, b) {
+ //       return a + b;
+ //   }/
+//}
+
+//module.exports = Calculator;
